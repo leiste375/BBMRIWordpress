c3606335e0b72a8d0ded1247ed44fb2323d756fe
leiste375 <108345514+leiste375@users.noreply.github.com>
2023-11-20 15:30:10 +0100

    Update url-replace.js
    Remove hardcoded domain to make script more flexible.

--- SFLLinksInject/js/url-replace.js
+++ SFLLinksInject/js/url-replace.js
@@ -1,22 +1,20 @@
 jQuery(document).ready(function($) {
-    // Check the current URL to restrict the execution of the script
-    if (window.location.href.indexOf("https://d1u.c1c.myftpupload.com/intranet/") !== -1) {
-        var urlPattern1 = /https:\/\/d1uc1c\.n3cdn1\.secureserver\.net\/ee-get-file\//g;
-        //var urlPattern2 = /https:\/\/d1u\.c1c\.myftpupload\.com\/ee-get-file\//g;
+    //Read current domain. Base URL hardcoded to https://example.com/intranet/.
+    const domain = window.location.host;
+    const baseurl = 'https://' + domain + '/intranet/';
 
-        // Function to replace the URLs on the page
-        function replaceURLs() {
-            $('a').each(function() {
-                var currentHref = $(this).attr('href');
+    if (window.location.href.indexOf(baseurl) !== -1) {
+        $('a').each(function() {
+            var currentHref = $(this).attr('href');
 
-                if (urlPattern1.test(currentHref)) {
-                    var newHref = currentHref.replace(urlPattern1, "https://d1u.c1c.myftpupload.com/ee-get-file/");
-                    $(this).attr('href', newHref);
-                }
-            });
-        }
-
-        // Call the function to replace URLs
-        replaceURLs();
+            if (currentHref.includes('/ee-get-file/') && currentHref.indexOf(baseurl) === -1) {
+                //Save string after ee-get-file.
+                var urlParts = currentHref.split('/ee-get-file/');
+                
+                //Construct URL to forego CDN.
+                var newHref = 'https://' + domain + '/ee-get-file/' + urlParts[1];
+                $(this).attr('href', newHref);
+            }
+        });
     }
 });
