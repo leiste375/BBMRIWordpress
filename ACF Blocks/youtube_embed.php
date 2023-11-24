e371ecdd90e13ee0e240bb6d44bec82cd7c8957d
leiste375 <108345514+leiste375@users.noreply.github.com>
2023-11-24 14:19:29 +0100

    Update youtube_embed.php
    Width and Height set via direct input from ACF

--- ACF Blocks/youtube_embed.php
+++ ACF Blocks/youtube_embed.php
@@ -19,18 +19,32 @@
 		<div class="ytembed-inner">
 			<p>
 			<?php
-			#Read field input. 
+			#Childproofing field input. Why is this necessary and not handled by a plugin dedicated to custom blocks.
             $yt = get_field('youtube_url');
-			#Match youtube embed code. 
+			if (!empty(get_field('yt_height'))) {
+                $yt_height = get_field('yt_height');
+            } else {
+                $yt_height = "315";
+            } if (!empty(get_field('yt_width'))) {
+                $yt_width = get_field('yt_width');
+            } else {
+                $yt_width = "560";
+            }
+			#If iframe code is detected, overwrite width and height.
             if (preg_match("/iframe/i", $yt)) {
-                echo $embedcode = $yt;
+				$embedcode_temp = preg_replace_callback('/width="(\d+)"/', function($match) use (&$yt_width) {
+					return 'width="' . $yt_width . '"';
+				}, $yt);
+				$embedcode_final = preg_replace_callback('/height="(\d+)"/', function($match) use (&$yt_height) {
+					return 'height="' . $yt_height . '"';
+				}, $embedcode_temp);
+                echo $embedcode_final;
 			} else {
                 #Regex to match all possible combinations, extract Youtube ID and finally construct the correct embed code. 
                 #Source: https://stackoverflow.com/questions/2936467/parse-youtube-video-id-using-preg-match/6382259#6382259
-                $embedcode1 = '<iframe width="560" height="315" src="https://www.youtube.com/embed/';
-                $embedcode2 = '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-                preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $yt, $yt_code);
-                echo $embedcode = $embedcode1 .= $yt_code[1] .= $embedcode2;
+				$pattern_url = '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i';
+                preg_match($pattern_url, $yt, $yt_code);
+                echo $embedcode = "<iframe width=\"$yt_width\" height=\"$yt_height\" src=\"https://www.youtube.com/embed/$yt_code[1]\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
 			} ?>
 			</p>
 		</div>
