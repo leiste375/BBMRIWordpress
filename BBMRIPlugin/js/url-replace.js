510a72be7da0822fb1d75e72b8b817b9f9a6c916
leiste375 <108345514+leiste375@users.noreply.github.com>
2024-02-02 18:24:50 +0100

    Add fail-safe for illegal characters

--- BBMRIPlugin/js/url-replace.js
+++ BBMRIPlugin/js/url-replace.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
     //Read current domain. Base URL hardcoded.
+    let inputcheck = ['', 'eeSFL_FileNameNew']
     const domain = window.location.host;
     const currentURL = window.location.href;
     if (currentURL.includes("/de/")) {
@@ -8,11 +9,27 @@ jQuery(document).ready(function($) {
         var baseurl = 'https://' + domain + '/home/intranet/';
     }
 
+    var pattern_count = 0;
+    function updatePattern(element) {
+        const currentName = $(element).attr('name');
+        if (currentName === 'eeSFL_NewFolderName') {
+            //$(element).attr('required', 'required');
+            $(element).attr('pattern', '^[a-zA0-9_\\-]*$');
+            $(element).attr('title', 'Only alphanumeric characters, as well as _ and - allowed');
+        } else if (currentName === 'eeSFL_FileNameNew') {
+
+        } else if (pattern_count > 0) {
+            $(element).removeAttr('pattern');
+            $(element).removeAttr('title');
+        } else {}
+    }
+
     if (currentURL.indexOf(baseurl) !== -1) {
+        //Include links and copy link button.
         $('a').each(function() {
             var currentHref = $(this).attr('href');
             var currentCopyLink = $(this).attr('onclick');
-            //Split URL and use latter part to construct working URL w/o CDN.
+            //Check if URL fits CDN or original domain name, split URL and use latter part to construct working URL w/o CDN.
             if (currentHref && currentHref.includes('/ee-get-file/') && currentHref.indexOf(baseurl) === -1) {
                 var urlParts = currentHref.split('/ee-get-file/');
                 var newHref = 'https://' + domain + '/ee-get-file/' + urlParts[1];
@@ -22,6 +39,37 @@ jQuery(document).ready(function($) {
                 var urlParts = currentCopyLink.split('/ee-get-file/');
                 var newCopyLink = 'eeSFL_CopyLinkToClipboard(\'https://' + domain + '/ee-get-file/' +urlParts[1];
                 $(this).attr('onclick', newCopyLink)
+            }
+        });
+        
+        //Insert pattern into relevant forms once on loading page.
+        $('input[type="text"]').each(function() {
+                console.log("This should appear only once");
+                updatePattern(this);
+                pattern_count += 1;
+        });
+
+        //Observe change in FileOpsActions to remove and re-add pattern as needed.
+        const targetElement = document.getElementById('eeSFL_FileOpsActionInput');
+        const observer = new MutationObserver(function(mutationsList) {
+            mutationsList.forEach(function(mutation) {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'name') {
+                    updatePattern(targetElement);
+                }
+            });
+        });
+        const observerConfig = { attributes: true };
+        observer.observe(targetElement, observerConfig);
+
+        document.getElementById('eeSFL_FileNameNew').addEventListener('input', function() {
+            var inputField = this.value;
+            var pattern = /^[a-zA-Z0-9-_]+(\.[a-zA-Z0-9-_]+)?\.(avi|bmp|csv|doc|docm|docx|jpeg|jpg|mov|mp3|mp4|msg|odp|ods|odt|pdf|png|ppt|pptm|pptx|svg|tif|tiff|tsv|txt|vsd|xls|xlsm|xlsx|zip)$/;
+            var pattern2 = /^[a-zA-Z0-9-_]*$/;
+            
+            if (pattern.test(inputField)) {
+            } else if (pattern2.test(inputField)) {
+            } else {
+                alert('Invalid input. Only alphanumeric characters, as well as \'_\' and \'-\' allowed. Use "File Nice Name" for complex names.');
             }
         });
     }
